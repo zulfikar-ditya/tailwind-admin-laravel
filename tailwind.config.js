47c3f432891ae850bfa46908dd6e7d3798a338b1
Zulfikar Ditya <zulfikarditya.work@gmail.com>
2022-09-22 21:38:51 +0700

    dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ["Nunito", ...defaultTheme.fontFamily.sans],
             },
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    darkMode: "class",
+
+    plugins: [require("@tailwindcss/forms"), require("flowbite/plugin")],
 };
