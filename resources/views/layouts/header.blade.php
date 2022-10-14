0342277a4c6e2bd611b532fb8a64c93b7ff9cc78
2-Q <243dwiki.com@gmail.com>
2022-10-14 13:38:35 +0700

    btn sidebar collapse on mobile

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,11 +1,11 @@
 <nav class="bg-sky-400 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-sky-800 z-50 shadow-md">
     <div class="container flex flex-wrap justify-between items-center mx-auto">
-        <button data-collapse-toggle="sidebar" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="sidebar" aria-expanded="false">
+        <div class="p-2 ml-3 text-gray-500" data-collapse-toggle="sidebar" aria-controls="sidebar" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
             <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
             </svg>
-        </button>
+        </div>
 
         <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
